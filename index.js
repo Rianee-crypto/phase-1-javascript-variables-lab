d6f6cc0c366d07da89e0f6696649ab3c206db8fb
Ainley Riany <ainley.riany@student.moringaschool.com>
2025-03-06 16:59:34 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+let companyCeo = 'Susan Smith';
+
+console.log(companyName);// Scuber
+console.log(mostProfitableNeighborhood);//Chelsea
+console.log(companyCeo); //Susan Smith
+
+
+
+
